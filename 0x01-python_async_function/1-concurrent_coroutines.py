03525117465c921c2c3237a365c1ec93400e8876
GirumZ <girum.zebe@gmail.com>
2024-05-07 00:51:44 +0300

    [Bug fix]: added asyncio.create_task

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -17,7 +17,7 @@ async def wait_n(n: int, max_delay: int):
     """
 
     delays = []
-    tasks = [wait_random(max_delay) for _ in range(n)]
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     for task in asyncio.as_completed(tasks):
         delay = await task
         delays.append(delay)
