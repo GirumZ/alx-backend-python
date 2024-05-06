bec50f3e6256b84b9d4398f4dee9101b36a00dda
GirumZ <girum.zebe@gmail.com>
2024-05-07 00:59:46 +0300

    [Bug fix]: Added type notation for the return value

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -16,12 +16,8 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
         list[float]: A list of all delays
     """
 
-    delays = []
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    for task in asyncio.as_completed(tasks):
-        delay = await task
-        delays.append(delay)
-    return delays
+    return [await task for task in asyncio.as_completed(tasks)]
 
 
 if __name__ == "__main__":
