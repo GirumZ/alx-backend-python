9c0e1af0bb53d5260b00c7e2f07849a5de8ce0bb
GirumZ <girum.zebe@gmail.com>
2024-05-27 14:28:31 +0300

    [Update]: Added new tests for _public_repos_url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,9 +3,10 @@
 Test module for client.GithubOrgClient class
 """
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -23,3 +24,13 @@ class TestGithubOrgClient(unittest.TestCase):
         test_response = org_test.org
         self.assertEqual(test_response, mock_org.return_value)
         mock_org.assert_called_once()
+
+    def test_public_repos_url(self):
+        """ Tests the result of _public_repos_url"""
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock:
+            payload = {"repos_url": "Hello World"}
+            mock.return_value = payload
+            test_class = GithubOrgClient('test')
+            result = test_class._public_repos_url
+            self.assertEqual(result, payload["repos_url"])
